4b22d980989eeeb75d6f2dddeb6eb581c3c660a0
Domino <zhikhanastasya@gmail.com>
2018-07-15 09:47:23 +0300

    Add file

---
+++ Logic.Task2/AccountOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Task2
+{
+    public static class AccountOperation
+    {
+        public static void Open(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Debit(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Credit(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Close(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
