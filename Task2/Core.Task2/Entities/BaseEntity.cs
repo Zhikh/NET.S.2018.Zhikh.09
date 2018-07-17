e913957478d6536bd8dbd01093ac5e6fb88713ee
Domino <zhikhanastasya@gmail.com>
2018-07-17 16:28:13 +0300

    Add file

---
+++ Task2/Core.Task2/Entities/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Task2.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
