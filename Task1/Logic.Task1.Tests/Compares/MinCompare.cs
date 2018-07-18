ef5139821fcf3d36c162df057d0699a04005d47f
Domino <zhikhanastasya@gmail.com>
2018-07-18 10:55:24 +0300

    Refactor (Logic.Task1.Tests): change access modificator

--- Task1/Logic.Task1.Tests/Compares/MinCompare.cs
+++ Task1/Logic.Task1.Tests/Compares/MinCompare.cs
@@ -1,6 +1,6 @@
 ﻿namespace Logic.Task1.Tests.Compares
 {
-    internal sealed class MinCompare : BaseCompare, ICompare<int[]> 
+    public sealed class MinCompare : BaseCompare, ICompare<int[]> 
     {
         internal override int GetCompareResult(int[] left, int[] right)
         {
