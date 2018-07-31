9996307fc94c739a0247c2761eea72e8416aaa1b
Domino <zhikhanastasya@gmail.com>
2018-07-31 23:56:44 +0300

    Add file

---
+++ Task2/Task2.BLL.Interface/Exceptions/InvalidAccountOperationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task2.BLL.Exceptions
+{
+    public class InvalidAccountOperationException : ApplicationException
+    {
+        public InvalidAccountOperationException()
+        {
+        }
+
+        public InvalidAccountOperationException(string message) : base(message)
+        {
+        }
+
+        public InvalidAccountOperationException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected InvalidAccountOperationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
