d382c16a2b48ef5a13ba77dea280493d2fcf61c6
Domino <zhikhanastasya@gmail.com>
2018-07-31 23:40:04 +0300

    Add file

---
+++ Task2/Task2.DependencyResolver/ResolverConfig.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Task2.Repositories;
+using Ninject;
+using Task2.BLL;
+using Task2.BLL.Interface.Services;
+using Task2.DAL.Fake.Strategies;
+using Task2.DAL.Interface.Repositories;
+using Task2.DAL.Interface.Strategies;
+
+namespace Task2.DependencyResolver
+{
+    public static class ResolverConfig
+    {
+        public static void ConfigurateResolver(this IKernel kernel)
+        {
+            kernel.Bind<IAccountService>().To<AccountService>();
+            kernel.Bind<IAccountRepository>().To<FakeAccountRepository>();
+            kernel.Bind<IPersonRepository>().To<FakePersonRepository>();
+            kernel.Bind<IAccountNumberGenerator<int>>().To<FakeAccountNumberGenerator>();
+        }
+    }
+}
