af26dfec3d6aefc83c4d25958fc9885acd4157c5
Domino <zhikhanastasya@gmail.com>
2018-08-22 22:35:32 +0300

    Add files

---
+++ Task2/Task2.BLL.Interface/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Task2.BLL.Interface.Entities;
+
+namespace Task2.BLL.Interface.Services
+{
+    interface IPersonService
+    {
+        void Create(Person person);
+
+        void Update(Person person);
+
+        void Delete(Person person);
+
+        void Get(string value);
+
+        IEnumerable<Person> GetAll();
+    }
+}
---
+++ Task2/Task2.BLL/Services/PersonService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.BLL.Interface.Entities;
+using Task2.BLL.Interface.Services;
+
+namespace Task2.BLL.Services
+{
+    public class PersonService : IPersonService
+    {
+        public void Create(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
