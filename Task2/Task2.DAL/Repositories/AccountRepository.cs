8791f2c0f744318c62d9148d90876ea9e79f30a9
Domino <zhikhanastasya@gmail.com>
2018-08-23 21:54:39 +0300

    Add files

---
+++ Task2/Task2.DAL/Repositories/AccountRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.DAL.Interface.Repositories;
+using Task2.DAL.Interfaces.DTO;
+
+namespace Task2.DAL.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public void Create(DalAccount entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(DalAccount entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DalAccount> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DalAccount GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DalAccount GetByPredicate(Expression<Func<DalAccount, bool>> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(DalAccount entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task2/Task2.DAL/Repositories/PersonRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.DAL.Interface.Repositories;
+using Task2.DAL.Interfaces.DTO;
+
+namespace Task2.DAL.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        public void Create(DalPerson entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(DalPerson entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DalPerson> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DalPerson GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DalPerson GetByPredicate(Expression<Func<DalPerson, bool>> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(DalPerson entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
