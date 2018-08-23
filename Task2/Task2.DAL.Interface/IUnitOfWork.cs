ec31d1ab404af02e80d7498141bba1a34d28d92f
Domino <zhikhanastasya@gmail.com>
2018-08-23 21:50:23 +0300

    Add files

---
+++ Task2/Task2.DAL.Interface/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Task2.DAL.Interface.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void SaveChanges();
+    }
+}
---
+++ Task2/Task2.DAL/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.Entity;
+using Task2.DAL.Interface.Repositories;
+
+namespace Task2.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool _disposed = false;
+
+        public UnitOfWork(DbContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public DbContext Context { get; private set; }
+
+        #region Dispose
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(Context));
+
+            if (disposing)
+                GC.SuppressFinalize(this);
+
+            if (Context != null)
+                Context.Dispose();
+
+            _disposed = true;
+        }
+
+        ~UnitOfWork()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+
+        public void SaveChanges()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
