f359a7746f9d4d10a7785a4173f96ea16d365809
Domino <zhikhanastasya@gmail.com>
2018-08-25 23:59:50 +0300

    Add (Task2.DAL) implementation

--- Task2/Task2.DAL/Repositories/PersonRepository.cs
+++ Task2/Task2.DAL/Repositories/PersonRepository.cs
@@ -1,49 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Task2.DAL.Interface.Repositories;
 using Task2.DAL.Interfaces.DTO;
+using Task2.DAL.Mappers;
+using Task2.ORM;
 
 namespace Task2.DAL.Repositories
 {
     public class PersonRepository : IPersonRepository
     {
+        private readonly DbContext context;
+
+        public PersonRepository(DbContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
         public void Create(DalPerson entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            var user = new Person()
+            {
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                MiddleName = entity.SecondName,
+                ContactData = new ContactData()
+                {
+                    Email = entity.Contact.Email
+                },
+                Passport = new Passport()
+                {
+                    PassportSeries = entity.SerialNumber
+                }
+            };
+
+            context.Set<Person>().Add(user);
         }
 
         public void Delete(DalPerson entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            var person = context.Set<Person>().Single(u => u.Id == entity.Id);
+
+            context.Set<Person>().Remove(person);
         }
 
         public IEnumerable<DalPerson> GetAll()
         {
-            throw new NotImplementedException();
+            return context.Set<Person>().Select(person => new DalPerson()
+            {
+                Id = person.Id,
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                SecondName = person.MiddleName,
+                SerialNumber = person.Passport.Number,
+                Contact = new DalContactData
+                {
+                    Email = person.ContactData.Email
+                }
+            });
         }
 
         public DalPerson GetById(int id)
         {
-            throw new NotImplementedException();
+            return context.Set<Person>().FirstOrDefault(person => person.Id == id).ToDalPerson(); ;
         }
 
-        public DalPerson GetByPredicate(Expression<Func<DalPerson, bool>> func)
+        public DalPerson GetByPredicate(Expression<Func<DalPerson, bool>> predicate)
         {
-            throw new NotImplementedException();
-        }
+            if (predicate == null) return null;
 
-        public void Save()
-        {
-            throw new NotImplementedException();
+            return context.Set<Person>().SingleOrDefault(predicate);
         }
 
         public void Update(DalPerson entity)
         {
-            throw new NotImplementedException();
+            var entityToUpdate = context.Set<Person>().First(e => e.Id == entity.Id);
+            return UpdateEntity(entityToUpdate, entity);
+        }
+
+        protected bool UpdateEntity(Person entityToUpdate, Person updatedEntity)
+        {
+            if (entityToUpdate == null) return false;
+            if (updatedEntity == null) return false;
+
+            try
+            {
+                context.Entry(entityToUpdate).CurrentValues.SetValues(updatedEntity);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
