c9ee46a55042bfd876cba671cf9c6fae57c86e37
Domino <zhikhanastasya@gmail.com>
2018-08-25 23:59:17 +0300

    Add file

---
+++ Task2/Task2.DAL/Mappers/PersonMappers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.DAL.Interfaces.DTO;
+using Task2.ORM;
+
+namespace Task2.DAL.Mappers
+{
+    public static class PersonMappers
+    {
+        public static DalPerson ToDalPerson(this Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Person ToDalPerson(this DalPerson person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
