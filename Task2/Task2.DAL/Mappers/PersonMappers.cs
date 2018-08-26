d3c62e37d1b2bf1a3f70460e5e4d2ddd0d271481
Domino <zhikhanastasya@gmail.com>
2018-08-27 00:25:22 +0300

    Add (Task2.DAL) implementation for PersonMapper

--- Task2/Task2.DAL/Mappers/PersonMappers.cs
+++ Task2/Task2.DAL/Mappers/PersonMappers.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Task2.DAL.Interfaces.DTO;
 using Task2.ORM;
 
@@ -12,12 +9,75 @@ namespace Task2.DAL.Mappers
     {
         public static DalPerson ToDalPerson(this Person person)
         {
-            throw new NotImplementedException();
+            if (person == null)
+            {
+                return null;
+            }
+
+            return new DalPerson
+            {
+                Id = person.Id,
+                FirstName = person.FirstName,
+                SecondName = person.MiddleName,
+                LastName = person.LastName,
+                SerialNumber = person.Passport.PassportSeries,
+                Contact = new DalContactData
+                {
+                    Email = person.ContactData.Email
+                },
+                Accounts = person.Accounts.ToDalAccounts()
+            };
         }
 
-        public static Person ToDalPerson(this DalPerson person)
+        public static IEnumerable<DalPerson> ToDalPersons(this IEnumerable<Person> person)
         {
-            throw new NotImplementedException();
+            return ToMany(person, ToDalPerson);
+        }
+
+        public static Person ToPerson(this DalPerson person)
+        {
+            if (person == null)
+            {
+                return null;
+            }
+
+            return new Person()
+            {
+                Id = person.Id,
+                FirstName = person.FirstName,
+                MiddleName = person.SecondName,
+                LastName = person.LastName,
+                Passport = new Passport
+                {
+                    PassportSeries = person.SerialNumber
+                },
+                ContactData = new ContactData
+                {
+                    Email = person.Contact.Email
+                },
+                Accounts = person.Accounts.ToAccounts()
+            };
+        }
+
+        public static IEnumerable<Person> ToPersons(this IEnumerable<DalPerson> person)
+        {
+            return ToMany(person, ToPerson);
+        }
+
+        private static ICollection<TTo> ToMany<TFrom, TTo>(IEnumerable<TFrom> accounts, Func<TFrom, TTo> func)
+        {
+            if (accounts == null)
+            {
+                return null;
+            }
+
+            var result = new List<TTo>();
+            foreach (var element in accounts)
+            {
+                result.Add(func(element));
+            }
+
+            return result;
         }
     }
 }
