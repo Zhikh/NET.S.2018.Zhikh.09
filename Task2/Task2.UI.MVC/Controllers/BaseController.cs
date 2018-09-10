d1bb526e7562ae48fe1a64f248fc6a271ec28e2f
Domino <zhikhanastasya@gmail.com>
2018-09-10 21:12:14 +0300

    Add files

---
+++ Task2/Task2.UI.MVC/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Task2.BLL.Interface.Services;
+using Ninject;
+
+namespace Task2.UI.MVC.Controllers
+{
+    public class BaseController : Controller
+    {
+        [Inject]
+        public IPersonService PersonService { get; set; }
+
+        [Inject]
+        public IAccountService AccountService { get; set; }
+
+        protected ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Task2/Task2.UI.MVC/Mappers/PersonMappers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Task2.BLL.Interface.Entities;
+using Task2.UI.MVC.Models.Person;
+
+namespace Task2.UI.MVC.Mappers
+{
+    public static class PersonMappers
+    {
+        public static Person ToPerson(this PersonCreateModel entity)
+        {
+            return new Person
+            {
+                FirstName = entity.FirstName,
+                SecondName = entity.MiddleName,
+                LastName = entity.LastName,
+                SerialNumber = entity.SerialNumber
+            };
+        }
+
+        public static PersonModel ToPersonModel(this Person entity)
+        {
+            return new PersonModel
+            {
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                AccountsCount = entity.Accounts.Count
+            };
+        }
+    }
+}
