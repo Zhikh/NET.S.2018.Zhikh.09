d7050505aadff035a2bfe02d0394d4c9d6397b7b
Domino <zhikhanastasya@gmail.com>
2018-09-10 23:16:11 +0300

    Add files

---
+++ Task2/Task2.UI.MVC/Models/Account/AccountDetailModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Task2.UI.MVC.Models.Account
+{
+    public class AccountDetailModel : AccountModel
+    {
+        public int Bonuses { get; set; }
+    }
+}
---
+++ Task2/Task2.UI.MVC/Models/Person/PersonDetailModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Task2.UI.MVC.Models.Account;
+
+namespace Task2.UI.MVC.Models.Person
+{
+    public sealed class PersonDetailModel : PersonCreateModel
+    {
+        public IEnumerable<AccountModel> Accounts;
+    }
+}
