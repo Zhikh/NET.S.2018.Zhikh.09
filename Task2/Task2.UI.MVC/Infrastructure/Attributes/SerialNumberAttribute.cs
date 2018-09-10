6c5439d4acee0baedb384b40995ac7b053a0f89e
Domino <zhikhanastasya@gmail.com>
2018-09-11 00:12:56 +0300

    Add (Task2.UI.MVC/Infrastructure) implementation for SerialNumberAttribute

--- Task2/Task2.UI.MVC/Infrastructure/Attributes/SerialNumberAttribute.cs
+++ Task2/Task2.UI.MVC/Infrastructure/Attributes/SerialNumberAttribute.cs
@@ -1,17 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+using System.Text.RegularExpressions;
 
 namespace Task2.UI.MVC.Infrastructure.Attributes
 {
     public class SerialNumberAttribute: ValidationAttribute
     {
+        private const string PATTERN = @"[^\d\s\p{P}\]{2}\d{6}";
+
         public override bool IsValid(object value)
         {
-            // TODO: realize
-            return false;
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var input = value as string;
+
+            if (input == null)
+            {
+                return false;
+            }
+
+            return Regex.IsMatch(input, PATTERN);
         }
     }
 }
