a87291911f63cfc67a218a62aa854ea36a09f1df
Domino <zhikhanastasya@gmail.com>
2018-09-10 22:04:13 +0300

    Add files

---
+++ Task2/Task2.UI.MVC/Infrastructure/HtmlHelpers/MessageHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Task2.UI.MVC.Infrastructure.HtmlHelpers
+{
+    public static class MessageHelper
+    {
+        public static string DisplayMessage(this HtmlHelper html, string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                return "";
+            }
+
+            return "@Html.Partial(\"_Message\")";
+        }
+    }
+}
